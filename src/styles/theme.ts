31d632470d51bba3dc66740bf42a8bb68bafe497
LOFyz <leonardo_farias2010@hotmail.com>
2021-10-13 02:20:04 -0300

    Chore: Theme changed

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,15 +1,19 @@
 import { createTheme } from '@material-ui/core/styles'
-import { amber, lightBlue, red } from '@material-ui/core/colors'
+import { lightBlue, red } from '@material-ui/core/colors'
 
 // Create a theme instance.
 const theme = createTheme({
   typography: { fontSize: 25 },
   palette: {
     primary: {
-      main: lightBlue[500]
+      main: '#66BB6A',
+      light: '#98EE99',
+      dark: '#338A3E'
     },
     secondary: {
-      main: amber[400]
+      main: '#D32F2F',
+      light: '#FF6659',
+      dark: '#9A0007'
     },
     error: {
       main: red.A400
