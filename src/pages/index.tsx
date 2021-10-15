3c40c23d390a32463ebb120225f0c84fde119b6f
LOFyz <leonardo_farias2010@hotmail.com>
2021-10-14 22:01:00 -0300

    Feat: Body added to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
+import styled from 'styled-components'
+import Body from '../components/Body'
 import Header from '../components/Header'
 
 const Home: React.FC = () => {
-  return <Header pageName="Dashboard" />
+  return (
+    <Container>
+      <Header pageName="Dashboard" />
+      <Body>
+        <h1 style={{ color: '#000', fontSize: 100 }}>OI</h1>
+      </Body>
+    </Container>
+  )
 }
 
 export default Home
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`
