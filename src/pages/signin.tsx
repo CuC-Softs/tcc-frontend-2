3f4a475803590325c94f5d161e096613f7d12c17
LOFyz <leonardo_farias2010@hotmail.com>
2021-10-14 23:46:20 -0300

    Feat: Form schema, validation and show errors

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-use-before-define
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Grid,
   Paper,
@@ -14,11 +14,15 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Checkbox from '@material-ui/core/Checkbox'
 import styled from 'styled-components'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import theme from '../styles/theme'
 
 const useStyles = makeStyles((theme) => ({
   paperStyle: {
     padding: 20,
     height: '70vh',
+    minHeight: 290,
     minWidth: 280,
     width: '25vw',
     margin: '20px auto',
@@ -33,19 +37,75 @@ const useStyles = makeStyles((theme) => ({
   radio: { lineHeight: 'normal' }
 }))
 
+interface LoginProps {
+  username: string
+  password: string
+  remember?: boolean | false
+}
+
+const initialValues: LoginProps = {
+  username: '',
+  password: '',
+  remember: false
+}
+
+const schema = Yup.object().shape({
+  username: Yup.string().required('Required Field'),
+  password: Yup.string().required('Required Field')
+})
+
 const SignIn: React.FC = () => {
   const classes = useStyles()
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2))
+    },
+    validationSchema: schema
+  })
+
+  const [formHaveError, setFormHaveError] = useState(false)
+
+  useEffect(() => {
+    if (
+      (!!formik.errors.username && formik.touched.username) ||
+      (!!formik.errors.password && formik.touched.password)
+    ) {
+      setFormHaveError(true)
+    } else {
+      if (formHaveError) {
+        setFormHaveError(false)
+      }
+    }
+  }, [formik.errors, formik.touched])
+
   return (
     <Container>
       <Grid>
         <Paper elevation={5} className={classes.paperStyle}>
           <Grid>
-            <Avatar className={classes.avatarStyle}>
+            <Avatar
+              className={classes.avatarStyle}
+              style={{
+                backgroundColor: formHaveError
+                  ? theme.palette.error.main
+                  : undefined
+              }}
+            >
               <LockOutlinedIcon />
             </Avatar>
             <h2>Sign In</h2>
           </Grid>
           <TextField
+            id="username"
+            name="username"
+            onChange={formik.handleChange}
+            autoComplete="username"
+            error={formik.touched.username && !!formik.errors.username}
+            helperText={formik.touched.username && formik.errors.username}
+            autoFocus
+            value={formik.values.username}
             label="Username"
             placeholder="Enter username"
             fullWidth
@@ -53,6 +113,14 @@ const SignIn: React.FC = () => {
             InputProps={{ inputProps: { style: { paddingLeft: 10 } } }}
           />
           <TextField
+            id="password"
+            name="password"
+            onChange={formik.handleChange}
+            autoComplete="password"
+            error={formik.touched.password && !!formik.errors.password}
+            helperText={formik.touched.password && formik.errors.password}
+            autoFocus
+            value={formik.values.password}
             label="Password"
             placeholder="Enter password"
             type="password"
@@ -61,19 +129,30 @@ const SignIn: React.FC = () => {
             InputProps={{ inputProps: { style: { paddingLeft: 10 } } }}
           />
           <FormControlLabel
-            control={<Checkbox name="checkedB" color="primary" />}
+            control={
+              <Checkbox
+                id="remember"
+                name="remember"
+                autoFocus
+                color="primary"
+                value={formik.values.remember}
+                onChange={formik.handleChange}
+              />
+            }
             label="Remember me"
             className={classes.radio}
           />
-          <Button
-            type="submit"
-            color="primary"
-            variant="contained"
-            className={classes.button}
-            fullWidth
-          >
-            Sign in
-          </Button>
+          <form noValidate onSubmit={formik.handleSubmit}>
+            <Button
+              type="submit"
+              color="primary"
+              variant="contained"
+              className={classes.button}
+              fullWidth
+            >
+              Sign in
+            </Button>
+          </form>
           <Typography>
             <Link href="#">Forgot password ?</Link>
           </Typography>
