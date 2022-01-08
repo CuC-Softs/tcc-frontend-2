5d008e6acbc7e1e06031d974ecb6e525cd5d74d2
LOFyz <leonardo_farias2010@hotmail.com>
2022-01-08 14:34:37 -0300

    feat: auth context

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,83 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import { setCookie, parseCookies, destroyCookie } from 'nookies'
+import Router from 'next/router'
+import api from '../services/api'
+import {
+  AuthContextType,
+  AuthResponse,
+  SignInData,
+  User
+} from './AuthContextTypes'
+import { toast } from 'react-toastify'
+
+const AuthContext = createContext({} as AuthContextType)
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  // Get user data if a cookie exists
+  useEffect(() => {
+    const { 'ifconnect.token': token } = parseCookies()
+
+    if (token && !user) {
+      const { 'ifconnect.user': userID } = parseCookies()
+
+      api.get<User>(`/users/${userID}`).then(({ data: user }) => {
+        setUser(user)
+      })
+    }
+  }, [])
+
+  async function signIn({ email, password }: SignInData): Promise<void> {
+    try {
+      setIsLoading(true)
+      const {
+        data: { token, user }
+      } = await api.post<AuthResponse>('/auth', {
+        email,
+        password
+      })
+
+      api.defaults.headers.common.Authorization = `Bearer ${token.token}`
+
+      setUser(user)
+
+      setCookie(undefined, 'ifconnect.token', token.token, {
+        expires: new Date(token.expires_at)
+      })
+
+      setCookie(undefined, 'ifconnect.user', String(user.id), {
+        expires: new Date(token.expires_at)
+      })
+      setIsLoading(false)
+    } catch (error) {
+      toast.error('Erro na autenticação', { position: 'top-right' })
+      setIsLoading(false)
+      console.log(error)
+    }
+  }
+
+  function logOut(): void {
+    destroyCookie(undefined, 'ifconnect.token', { path: '/' })
+    destroyCookie(undefined, 'ifconnect.user', { path: '/' })
+
+    Router.push('/')
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, signIn, logOut, isLoading }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext)
+
+  if (!context) {
+    throw new Error('useAuth must be used within a auth provider')
+  }
+
+  return context
+}
---
+++ src/contexts/AuthContextTypes.ts
@@ -0,0 +1,34 @@
+export type Token = {
+  type: string
+  token: string
+  expires_at: string // eslint-disable-line camelcase
+}
+
+export type User = {
+  id: number
+  email: string
+  username: string
+  name: string
+  remember_me_token: string | null // eslint-disable-line camelcase
+  member_id: number // eslint-disable-line camelcase
+  created_at: string // eslint-disable-line camelcase
+  updated_at: string // eslint-disable-line camelcase
+}
+
+export type SignInData = {
+  email: string
+  password: string
+  rememberMe: boolean
+}
+
+export type AuthResponse = {
+  token: Token
+  user: User
+}
+
+export type AuthContextType = {
+  user: User
+  signIn: (data: SignInData) => Promise<void>
+  logOut: () => void
+  isLoading: boolean
+}
